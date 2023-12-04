27906992d31b584a42e3d550fe357ba99421599b
Chris <Chris Nickell>
2023-12-04 07:38:23 -0700

    p2 is close

--- day04/day04.py
+++ day04/day04.py
@@ -18,35 +18,39 @@ for line in data:
     cards[card_no] = (winning, my_num)
 
 card_cal = defaultdict(int)
-all_cards = defaultdict(list)
-order_card = []
+matches = defaultdict(int)
+
 for card, nums in cards.items():
-    order_card.append(card)
     w, n = nums
     count = 0
-    matches = 0
     for i,j in product(w,n):
         if i == j:
             count = count *2 if count != 0 else 1
-            matches += 1
+            matches[card] += 1
     
     card_cal[card] = count
-    if matches:
-        for i in range(card+1, card+1+matches):
-            all_cards[card].append(i)
-
-    
 
 print(sum(card_cal.values()))
 
-alt = [card for card in card_cal.keys()]
-for card in order_card:
-    if card in all_cards.keys():
-        for vals in all_cards.values():
-            for i in vals:
-                alt.append(i)
-p2 = 0
-counter = defaultdict(int)
-for a in sorted(alt):
-    counter[a] +=1
-print(p2)
+cards_count = defaultdict(int)
+def get_next(i: int):
+    global cards_count, card_cal, matches
+    s = card_cal[i]
+    cards_count[i] += 1
+    if matches[i]:
+        for nc in range(i+1, i+1+matches[i]):
+            cards_count[nc] += 1
+            s += get_next(nc)
+    return s
+
+si = 0
+
+get_next(1)
+for card, num_card in cards_count.items():
+    si += card_cal[card] * num_card
+    print(f'card {card}: {num_card} Copies each for {card_cal[card]} = {card_cal[card] * num_card} (sum = {si})')
+for card in card_cal:
+    if card not in cards_count:
+        si += card_cal[card]
+        print(f'card {card}: 1 copies each for {card_cal[card]} = {card_cal[card]} (sum = {si})')
+print(si)
