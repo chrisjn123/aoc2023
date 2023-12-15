adc013a865aee32f769ca218c2a980ef2acd711f
Chris <Chris Nickell>
2023-12-14 22:42:16 -0700

    p2

--- day15/day15.py
+++ day15/day15.py
@@ -1,19 +1,12 @@
 from utils import get_data
-
-from collections import defaultdict
-from itertools import product
 from functools import lru_cache
-from concurrent.futures import ThreadPoolExecutor
+from time import perf_counter
+start = perf_counter()
 
 data = get_data('in.txt')
 data = data.split(',')
-'''
-Determine the ASCII code for the current character of the string.
-Increase the current value by the ASCII code you just determined.
-Set the current value to itself multiplied by 17.
-Set the current value to the remainder of dividing itself by 256.
-'''
 
+@lru_cache
 def get_hash(curr_str: str):
     curr_value = 0
     for ch in curr_str:
@@ -62,3 +55,4 @@ for box, item in boxes.items():
         v = (box+1) * (i+1) * fl
         s += v
 print(s)
+print(f"{perf_counter() - start}")
