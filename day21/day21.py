d5de74558eac53f2ac1145a759d6d3b36f3b54f8
Chris <Chris Nickell>
2023-12-21 12:02:02 -0700

    p1 but example works and real doesn't :)

--- day21/day21.py
+++ day21/day21.py
@@ -3,16 +3,10 @@ from heapq import heappush, heappop
 from itertools import product
 
 points = set()
-TARGET = 6
+filename = 'in.txt'
+TARGET = 6 if filename == 'test.txt' else 64
 
-def manhattan(x1: int, y1: int,
-              x2: int, y2: int) -> int:
-    global TARGET
-    ret = abs(x1-x2) + abs(y1-y2)
-    if ret == TARGET: print(f'({x1}, {y1}): {ret}')
-    return ret
-
-data = [list(line) for line in get_data('test.txt')]
+data = [list(line) for line in get_data(filename)]
 for i, line in enumerate(data):
     for j, ch in enumerate(line):
         if ch == 'S':
@@ -21,27 +15,30 @@ print(f'Starting point is: {start}')
 
 def search(state: tuple[int]):
     global TARGET, data
-    x,y = state
+    x, y = state
     seen = set()
     q = []
     heappush(q, (0,state))
 
     while len(q):
+        print(f'Length of queue: {len(q)}')
         score, (x,y) = heappop(q)
-        if not (0<=x<len(data)<0 or 0<=y<len(data[0])):
-            return
-        if (x,y) in seen or score > TARGET or data[x][y] == '#':
+        if x < 0 or x >= len(data) or y < 0 or y >= len(data) or score > TARGET or data[x][y] == '#':
             continue
-        seen.add((x,y))
 
         if score == TARGET and data[x][y] == '.':
             points.add((x,y))
             continue
-        
-        heappush(q, (score+1, (x-1,y)))
-        heappush(q, (score+1, (x+1,y)))
-        heappush(q, (score+1, (x,y-1)))
-        heappush(q, (score+1, (x,y+1)))
+        if (x,y) in seen:
+            continue
+        if data[x-1][y] == '.':
+            heappush(q, (score+1, (x-1,y)))
+        if data[x+1][y] == '.':
+            heappush(q, (score+1, (x+1,y)))
+        if data[x][y-1] == '.':
+            heappush(q, (score+1, (x,y-1)))
+        if data[x][y+1] == '.':
+            heappush(q, (score+1, (x,y+1)))
 
 search(start)
-print(len(points))
+print(len(points)+1)
